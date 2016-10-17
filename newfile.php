c5cca7384517e3047b76416a8bf3e432823a45ba
CM237 <cm237@njit.edu>
2016-10-17 18:43:51 -0400

    Update newfile.php

--- newfile.php
+++ newfile.php
@@ -1,3 +1,27 @@
+<html>
+  
 <?php
-  echo 'new file';
+    
+
+    const tree = "nintendo"; 
+    $tree1 = "mario";
+
+    $Array1 = array($tree1, tree, "mushroom");
+    $Array2 = array("star", "flower");
+    
+    $Array3 = array("0" => "mario", 
+                    "5" => "nintendo",
+                    "3" => "mushroom",
+                    "2" => "64",
+                    "tree" => "another stage",
+                    "nintendo" => "another game",
+                    );
+
+    print "<pre>"; 
+    print_r ($Array3);
+    print "</pre>"
+
+
 ?>
+
+</html>
