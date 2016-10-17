a29f033d32ab0b1bc48115264d395f6fa5144f58
CM237 <cm237@njit.edu>
2016-10-17 18:34:23 -0400

    Update Array.php

--- Array.php
+++ Array.php
@@ -1,1 +1,23 @@
+<?php
+    
 
+    const tree = "nintendo"; 
+    $tree1 = "mario";
+
+    $Array1 = array($tree1, tree, "mushroom");
+    $Array2 = array("star", "flower");
+    
+    $Array3 = array("0" => "mario", 
+                    "5" => "nintendo",
+                    "3" => "mushroom",
+                    "2" => 64
+                    "tree" => "another stage"
+                    "nintendo" => "another game"
+                    );
+
+    print "<pre>"; 
+    print_r ($Array3);
+    print "</pre>"
+
+
+?>
