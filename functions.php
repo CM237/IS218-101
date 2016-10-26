d522509227a8f8930d33de4159fc44367fc05ab2
Your Name <cm237@afsconnect1.njit.edu>
2016-10-25 20:06:53 -0400

    made hw changes

---
+++ functions.php
@@ -0,0 +1,28 @@
+<html>
+  <p>
+    <?php
+    //This is to test some functions and homework
+    // Get a partial string from within your own name
+    // and print it to the screen!
+    $myname = ("Carlos");
+    
+    $partial = substr($myname, 0, 3);
+    print $partial;
+    print $myname;
+    ?>
+  </p>
+  <p>
+    <?php
+    // Make your name upper case and print it to the screen:
+    $uppercase = strtoupper("Carlos");
+    print $uppercase;
+    ?>
+  </p>
+  <p>
+    <?php
+    // Make your name lower case and print it to the screen:
+    $lowercase = strtolower("Carlos");
+    print $lowercase;
+    ?>
+  </p>
+</html>
