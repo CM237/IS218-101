46a074c2869addf6f4776d6460a1fc9a4ee87dc2
Your Name <cm237@afsconnect1.njit.edu>
2016-11-02 15:55:34 -0400

    made arrays to html functio again

--- ArraystoHTML.php
+++ ArraystoHTML.php
@@ -1,9 +1,27 @@
-<php?
 
+<html>
+<body>
+<h1>ArraystoHTML CSV reasding assignments 3 and 4</h1>
 
-
-
-
+<?php 
+      
+	include "functionscsvtoarray.php";
+	$obj = new functionscsvtoarray;
+	
+	// Set path to CSV file
+	$csvFile = 'users.csv';
+	
+	$csvArray = $obj->readCSVtoArray($csvFile);
+	
+	$obj->toHTML2($csvArray);
+	
 ?>
+</body>
+</html>
+
+
+
+
+
 
 
