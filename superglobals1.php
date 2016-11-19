e1928e17006312408168de106e05708dccefabdd
Your Name <cm237@afsconnect1.njit.edu>
2016-11-19 03:22:10 -0500

    made some final changes

--- superglobals1.php
+++ superglobals1.php
@@ -1,5 +1,119 @@
-<php?
+<!DOCTYPE html>
+<html>
+<body>
 
+<?php 
+//SUPER GLOBAL EXAMPLE 1
+$x = 75;
+$y = 25; 
 
+function addition() {
+    $GLOBALS['z'] = $GLOBALS['x'] + $GLOBALS['y'];
+}
 
+addition();
+echo $z;
 ?>
+
+</body>
+</html>
+
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php 
+
+//$_SERVER is a PHP super global variable which holds information about headers, paths, and script locations.
+
+echo $_SERVER['PHP_SELF'];
+echo "<br>";
+echo $_SERVER['SERVER_NAME'];
+echo "<br>";
+echo $_SERVER['HTTP_HOST'];
+echo "<br>";
+echo $_SERVER['HTTP_REFERER'];
+echo "<br>";
+echo $_SERVER['HTTP_USER_AGENT'];
+echo "<br>";
+echo $_SERVER['SCRIPT_NAME'];
+?>
+
+</body>
+</html>
+
+<!DOCTYPE html>
+<html>
+<body>
+
+<form method="post" action="<?php echo $_SERVER['PHP_SELF'];?>">
+  Name: <input type="text" name="fname">
+  <input type="submit">
+</form>
+
+<?php
+//PHP $_REQUEST is used to collect data after submitting an HTML form.
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // collect value of input field
+    $name = $_REQUEST['fname']; 
+    if (empty($name)) {
+        echo "Name is empty";
+    } else {
+        echo $name;
+    }
+}
+?>
+
+</body>
+</html>
+
+<!DOCTYPE html>
+<html>
+<body>
+
+<form method="post" action="<?php echo $_SERVER['PHP_SELF'];?>">
+  Name: <input type="text" name="fname">
+  <input type="submit">
+</form>
+
+<?php
+
+//PHP $_POST is widely used to collect form data after submitting an HTML form with method="post". 
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // collect value of input field
+    $name = $_POST['fname']; 
+    if (empty($name)) {
+        echo "Name is empty";
+    } else {
+        echo $name;
+    }
+}
+?>
+
+</body>
+</html>
+
+<!DOCTYPE html>
+<html>
+<body>
+//testing $GET
+<a href="test_get.php?subject=PHP&web=W3schools.com">Test $GET</a>
+
+</body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
